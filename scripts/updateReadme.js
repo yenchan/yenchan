e7e2badc27e9fd9ca0afd7cc14f579ec8a908c54
Yen Chan <ypcny@yahoo.com>
2026-05-21 16:37:24 +0100

    scripts updateReadme.js

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -1,37 +1,45 @@
 const fs = require("fs");
+const path = require("path");
 
-const readmePath = "README.md";
+const readmePath = path.join(__dirname, "..", "README.md");
+const gameStatePath = path.join(__dirname, "..", "data", "gameState.json");
+
 const readme = fs.readFileSync(readmePath, "utf8");
-const gameState = JSON.parse(fs.readFileSync("data/gameState.json", "utf8"));
+const gameState = JSON.parse(fs.readFileSync(gameStatePath, "utf8"));
 
-let statusText = `Current turn: ${gameState.currentPlayer === "gold" ? "Gold" : "Espresso"}`;
+const imageVersion = gameState.moveHistory.length;
+
+const playerNames = {
+  gold: "Espresso",
+  red: "Rose"
+};
+
+let statusLine = `**Current turn: ${playerNames[gameState.currentPlayer] || gameState.currentPlayer}**`;
 
 if (gameState.winner) {
-  statusText = `Winner: ${gameState.winner === "gold" ? "Gold" : "Espresso"}`;
+  statusLine = `**Winner: ${playerNames[gameState.winner] || gameState.winner}**`;
+} else if (gameState.isDraw) {
+  statusLine = `**Result: Draw**`;
 }
 
-if (gameState.isDraw) {
-  statusText = "Game ended in a draw";
-}
+const connect4Section = `<!-- CONNECT4:START -->
+![Connect 4 board](./assets/connect4-board.svg?v=${imageVersion})
 
-const newSection = `
-![Connect 4 board](./assets/connect4-board.svg)
-
-**${statusText}**
+${statusLine}
 
 **Make a move:**  
-[1](../../issues/new?template=connect4-move.yml&title=connect4-move-1) ·
-[2](../../issues/new?template=connect4-move.yml&title=connect4-move-2) ·
-[3](../../issues/new?template=connect4-move.yml&title=connect4-move-3) ·
-[4](../../issues/new?template=connect4-move.yml&title=connect4-move-4) ·
-[5](../../issues/new?template=connect4-move.yml&title=connect4-move-5) ·
-[6](../../issues/new?template=connect4-move.yml&title=connect4-move-6) ·
-[7](../../issues/new?template=connect4-move.yml&title=connect4-move-7)
-`;
+[1](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-1&column=1) ·
+[2](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-2&column=2) ·
+[3](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-3&column=3) ·
+[4](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-4&column=4) ·
+[5](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-5&column=5) ·
+[6](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-6&column=6) ·
+[7](https://github.com/yenchan/yenchan/issues/new?template=connect4-move.yml&title=connect4-move-7&column=7)
+<!-- CONNECT4:END -->`;
 
 const updatedReadme = readme.replace(
   /<!-- CONNECT4:START -->[\s\S]*<!-- CONNECT4:END -->/,
-  `<!-- CONNECT4:START -->${newSection}<!-- CONNECT4:END -->`
+  connect4Section
 );
 
 fs.writeFileSync(readmePath, updatedReadme);
