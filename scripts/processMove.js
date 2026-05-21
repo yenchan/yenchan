f9859039503321a1f79ddb89ddf821bf6af798e1
Yen Chan <ypcny@yahoo.com>
2026-05-21 14:50:03 +0100

    Update processMove.js
    Fix processMove parsing

--- scripts/processMove.js
+++ scripts/processMove.js
@@ -1,1 +1,20 @@
+const issueTitle = process.env.ISSUE_TITLE || "";
+const issueBody = process.env.ISSUE_BODY || "";
 
+let column = null;
+
+const titleMatch = issueTitle.match(/connect4-move-(\d+)/i);
+if (titleMatch) {
+  column = Number(titleMatch[1]);
+}
+
+if (!column) {
+  const bodyMatch = issueBody.match(/\b([1-7])\b/);
+  if (bodyMatch) {
+    column = Number(bodyMatch[1]);
+  }
+}
+
+if (!column || column < 1 || column > 7) {
+  throw new Error(`Could not determine a valid column from issue. Title: ${issueTitle} Body: ${issueBody}`);
+}
